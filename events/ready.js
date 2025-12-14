b37ef31f999ec5d14e1c396aab93f7a1d60611e6
Behildus <99230702+Behildus@users.noreply.github.com>
2025-12-14 15:38:46 -0500

    remove debugging

--- events/ready.js
+++ events/ready.js
@@ -30,9 +30,9 @@ module.exports = {
 		setInterval(function () {
 			var date = new Date();
 			var time = date.getDay().toString() + ":" + date.getHours().toString() + ":" + date.getMinutes().toString();
-			console.log(time);
+			//console.log(time);
 			for (const message of schedule.messages) {
-				console.log(message.time);
+				//console.log(message.time);
 				if (message.time == time) {
 					//console.log(channel.guildId);
 					if (channel.guildId == message.server) {
